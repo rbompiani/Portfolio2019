f7c8fdc24ec21bce15ca5bbd01bfbaeaf262f4f2
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-07-07 18:37:50 -0700

    added project json file, populated projects and added styling based on json properties

---
+++ assets/javascript/logic.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+    $.getJSON( "data/projects.json", function( data ) {
+        var projects = data.projects;
+        $.each( projects, function( i ) {
+            var newArt = $("<article>");
+            newArt.addClass("card").addClass(this.major_skill);
+            newArt.append("<div class='image' style='background-image: url(assets/images/"+this.img+")'>");
+            var newSnap = $("<div>");
+            newSnap.addClass("snapshot");
+            newSnap.append("<h2 class='light'>"+this.title+"</h2>");
+            newSnap.append("<p class='light'>"+this.short_desc+"</p>");
+            newSnap.append("<a class='light' href='#'>More -></a> ");
+            newArt.append(newSnap);           
+            var skillBlock = $("<div class='skillBlock'></div>")
+            $.each(this.skills, function(e){
+                skillBlock.append("<div class='skill "+this+"'>"+this.charAt(0)+"</div>");
+                console.log(this);
+            })
+            newArt.append(skillBlock);
+            $("#main").append(newArt);
+        });
+    });
+
+
+    
+});
+
+/*
+
+var newArt = $("<article>");
+    newArt.addClass("card");
+    $("#main").append(newArt);
+    console.log("were' in the wrong spot");
+
+
+<article class="card">
+<div class="image" style="background-image: url(assets/images/smalltest.jpg)">
+</div>  
+<div class="snapshot">
+    <h2 class="light">Article Title</h2>
+    <p class="light">This is a sample description for the project.</p>   
+    <a class="light" href="#">More -></a>                         
+</div>
+
+
+</article>
+*/
